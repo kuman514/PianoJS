f18c7907f113f9501cc55591c5e2f25cbf81f856
kumankoishi.py <7iggif@naver.com>
2022-01-22 17:29:30 +0900

    Detached key event from app

--- js/App.js
+++ js/App.js
@@ -1,10 +1,11 @@
+import { addKey } from './Keys';
+
 (function App() {
   const keyName = [
     'KeyZ', 'KeyS', 'KeyX', 'KeyD', 'KeyC',
     'KeyV', 'KeyG', 'KeyB', 'KeyH', 'KeyN', 'KeyJ', 'KeyM', 
     'Comma', 'KeyL', 'Period', 'Semicolon', 'Slash'
   ];
-  const keys = {};
 
   for (let i = 0; i < 17; i++) {
     const keyElem = document.querySelector(`#key-${i}`);
@@ -24,23 +25,6 @@
     keyElem.addEventListener('mousedown', onDown);
     keyElem.addEventListener('mouseup', onUp);
 
-    Object.assign(keys, {
-      [keyName[i]]: {
-        onDown: onDown,
-        onUp: onUp
-      }
-    });
+    addKey(keyName[i], onUp, onDown);
   }
-
-  document.addEventListener('keydown', (event) => {
-    if (keys[event.code]) {
-      keys[event.code].onDown();
-    }
-  });
-
-  document.addEventListener('keyup', (event) => {
-    if (keys[event.code]) {
-      keys[event.code].onUp();
-    }
-  });
 })();
---
+++ js/Keys.js
@@ -0,0 +1,22 @@
+const keys = {};
+
+export function addKey(keyName, onUp, onDown) {
+  Object.assign(keys, {
+    [keyName]: {
+      onDown: onDown,
+      onUp: onUp
+    }
+  });
+}
+
+document.addEventListener('keydown', (event) => {
+  if (keys[event.code]) {
+    keys[event.code].onDown();
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (keys[event.code]) {
+    keys[event.code].onUp();
+  }
+});
